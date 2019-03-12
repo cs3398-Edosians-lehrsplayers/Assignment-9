9429870dd9c0a76c83f73f3f674181655f5d5bb5
John Pappas <jaydeepappas@gmail.com>
2019-03-11 21:36:13 -0500

    Added icar and basic car

---
+++ BasicCar.java
@@ -0,0 +1,12 @@
+public class BasicCar implements ICar {
+
+    public BasicCar() {
+		System.out.print(" BasicCar Constructor\n");
+	}
+
+    @Override
+    public String GetDescription() {
+        return "A Basic Car";
+    }
+}
+
