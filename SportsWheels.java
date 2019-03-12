62b00f079ea3dbaee5dc0d113b034cd308f23ccd
codewithjaysen <32177778+codewithjaysen@users.noreply.github.com>
2019-03-11 21:34:03 -0500

    SportsWheels.java

---
+++ SportsWheels.java
@@ -0,0 +1,23 @@
+
+
+
+public class SportsWheels extends CarOptions
+{
+	
+   public String Wheels;
+
+
+    public SportsWheels()
+    {
+      System.out.print(" CarWithSportsWheels Constructor\n");
+			Wheels = "Wheels";
+    }
+
+    @Override
+    public String GetDescription()
+  	{
+        return super.GetDescription() + " with " + Wheels;
+  	}
+
+
+}
