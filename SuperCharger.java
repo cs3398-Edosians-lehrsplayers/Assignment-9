8dcb1eb43b639212c9debd330216fe9a37c55d38
John Pappas <jaydeepappas@gmail.com>
2019-03-11 21:28:58 -0500

    Create SuperCharger.java

---
+++ SuperCharger.java
@@ -0,0 +1,21 @@
+
+package carcraft;
+
+public class SuperCharger extends CarOptions
+{
+        public String SuperCharger;
+
+
+        public SuperCharger(String superCharger)
+        {
+            System.out.println(" SuperCharger Constructor\n");
+
+            SuperCharger = superCharger;
+        }
+
+        @Override
+        public String GetDescription()
+        {
+            return super.GetDescription() + " with " + SuperCharger + " color";
+        }
+}
